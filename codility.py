7238231a29dbf87cec452ef3b010972a6df013ae
uros96 <34546412+uros96@users.noreply.github.com>
2018-05-12 02:45:20 +0200

    lesson 3 finished

--- codility.py
+++ codility.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 # BinaryGap - Find maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N
 def binary_gap(N):
@@ -45,9 +46,28 @@ def perm_missing_elem(A):
             if A[i+1]-A[i] is 2:
                 missing = A[i]+1
     return A[len(A)-1]+1 if missing is 0 else missing 
-    
+
+# FrogJmp - Count minimal number of jumps from position X to Y.
+def frog_jmp(X, Y, D):
+    return math.ceil((Y-X)/D)
+
+# TapeEquilibrium - Minimize the value |(A[0] + ... + A[P-1]) - (A[P] + ... + A[N-1])|.
+def tape_equilibrium(A):
+    B=[]
+    suma = sum(A)
+    temp_suma = 0
+    for p in range(0, len(A)-1):
+        temp_suma+=A[p]
+        a = temp_suma
+        b = suma-temp_suma
+        B.append(a-b) if a-b>0 else B.append(b-a) 
+    B.sort()
+    return B[0]
+
 if __name__ is "__main__":
     print(binary_gap(1025))
     print(cyclic_rotation([1,2,3,4,5,6,7,8], 10))
     print(odd_occurrences_in_array([9,3,9,3,9,7,9]))
     print(perm_missing_elem([1,3]))
+    print(frog_jmp(10, 85, 30))
+    print(tape_equilibrium([3,1,2,4,3]))
